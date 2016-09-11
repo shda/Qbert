157363c519f5d813f5d6627fe1f7430d13dd2f27
shda <arheuser@gmail.com>
2016-09-12 01:10:47 +0300

    New map

---
+++ Assets/Qbert/Scripts/Editor/CreateMapAsset.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+
+public class CreateMapAsset : MonoBehaviour 
+{
+    [MenuItem("Assets/Create map asset")]
+    static void CreateSettingsAsset()
+    {
+        CreateAsset<MapAsset>("MapAsset");
+    }
+
+    static private void CreateAsset<T>(String name) where T : ScriptableObject
+    {
+        var dir = "Assets/Configuration/";
+        var selected = Selection.activeObject;
+        if (selected != null)
+        {
+            var assetDir = AssetDatabase.GetAssetPath(selected.GetInstanceID());
+            if (assetDir.Length > 0 && Directory.Exists(assetDir))
+                dir = assetDir + "/";
+        }
+        ScriptableObject asset = ScriptableObject.CreateInstance<T>();
+        AssetDatabase.CreateAsset(asset, dir + name + ".asset");
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+}
---
+++ Assets/Qbert/Scripts/Editor/MapEditor/MapEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MapEditor : EditorWindow
+{
+    //[MenuItem("Map/Map editor")]
+    /*
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(BuidingWindow));
+    }
+    */
+    void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
