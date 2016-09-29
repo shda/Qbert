491fdbd1db085409c4169beee3ae84fca4dc6842
shda <arheuser@gmail.com>
2016-09-29 04:56:43 +0300

    Configuration control buttons.

---
+++ Assets/Qbert/Scripts/ControlConfiguratorScripts/ButtonMove.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonMove : MonoBehaviour , IDragHandler ,
+    IPointerDownHandler ,IPointerUpHandler
+{
+    public Image centerImage;
+    public Button button;
+
+    public Action<ButtonMove> OnButtonStartDrag;
+    public Action<ButtonMove> OnButtonEndDrag;
+    public Action<ButtonMove> OnButtonDrag;
+
+    public RectTransform parentRect;
+    public RectTransform rectTransform;
+
+    public Color intersectColor;
+
+    private Vector2 defaultPosition;
+    private Color defaultColor;
+
+    public Bounds bounds
+    {
+        get
+        {
+            return CalculateBounds(rectTransform);
+        }
+    }
+
+    Bounds CalculateBounds(RectTransform transform)
+    {
+        Bounds bounds = new Bounds(transform.localPosition, new Vector3(transform.rect.width, transform.rect.height, 0.0f));
+        return bounds;
+    }
+
+    public void SavePosition()
+    {
+        var pos = transform.localPosition;
+
+        PlayerPrefs.SetFloat(name + "X", pos.x);
+        PlayerPrefs.SetFloat(name + "Y", pos.y);
+    }
+
+    public void LoadPosition()
+    {
+        float x = PlayerPrefs.GetFloat(name + "X", defaultPosition.x);
+        float y = PlayerPrefs.GetFloat(name + "Y", defaultPosition.y);
+        transform.localPosition = new Vector3(x,y,0);
+    }
+
+    void Start()
+    {
+        defaultColor = centerImage.color;
+        defaultPosition = transform.localPosition;
+        button.enabled = false;
+
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void SetIntersect()
+    {
+        centerImage.color = intersectColor;
+    }
+
+    public void ResetIntersect()
+    {
+        centerImage.color = defaultColor;
+    }
+	
+	void Update () 
+	{
+	    
+	}
+
+    public void SetButtonToDefaultPosition()
+    {
+        transform.localPosition = defaultPosition;
+        SavePosition();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        var screnPos = eventData.pointerCurrentRaycast.screenPosition;
+
+        Vector2 resultRect;
+        
+        bool isOk = RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRect, screnPos,
+            eventData.enterEventCamera,out resultRect);
+
+        if (isOk)
+        {
+            transform.localPosition = resultRect;
+        }
+
+        if (OnButtonDrag != null)
+        {
+            OnButtonDrag(this);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (OnButtonStartDrag != null)
+        {
+            OnButtonStartDrag(this);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (OnButtonEndDrag != null)
+        {
+            OnButtonEndDrag(this);
+        }
+    }
+}
---
+++ Assets/Qbert/Scripts/ControlConfiguratorScripts/ControlConfigurator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class ControlConfigurator : MonoBehaviour
+{
+    public ButtonMove[] buttons;
+    public Transform buttonSave;
+    public Transform textDetectIntersectionButtons;
+
+    private void ConnectEvents()
+    {
+        foreach (var buttonMove in buttons)
+        {
+            buttonMove.OnButtonStartDrag = OnButtonStartDrag;
+            buttonMove.OnButtonEndDrag = OnButtonEndDrag;
+            buttonMove.OnButtonDrag = OnButtonDrag;
+        }
+    }
+
+    private void OnButtonDrag(ButtonMove buttonMove)
+    {
+        CheckIntersectButtons();
+    }
+
+    public void OnSave()
+    {
+        foreach (var buttonMove in buttons)
+        {
+            buttonMove.SavePosition();
+        }
+    }
+
+    public void OnCancel()
+    {
+       
+    }
+
+    public void LoadButtonsPositions()
+    {
+        foreach (var buttonMove in buttons)
+        {
+            buttonMove.LoadPosition();
+        }
+    }
+
+    public void OnDefault()
+    {
+        foreach (var buttonMove in buttons)
+        {
+            buttonMove.SetButtonToDefaultPosition();
+        }
+
+        CheckIntersectButtons();
+    }
+
+    private void OnButtonEndDrag(ButtonMove buttonMove)
+    {
+        CheckIntersectButtons();
+    }
+    public void CheckIntersectButtons()
+    {
+        bool isIntersectButtons = false;
+
+        foreach (var button in buttons)
+        {
+            button.ResetIntersect();
+        }
+
+        foreach (var button in buttons)
+        {
+            foreach (var button2 in buttons)
+            {
+                if(button2 == button)
+                    continue;
+
+                if (button.bounds.Intersects(button2.bounds))
+                {
+                    button.SetIntersect();
+                    button2.SetIntersect();
+                    isIntersectButtons = true;
+                }
+            }
+        }
+
+        if (isIntersectButtons)
+        {
+            ShowTextDetectIntersectionButtons();
+        }
+        else
+        {
+            HideTextDetectIntersectionButtons();
+        }
+    }
+
+    private void OnButtonStartDrag(ButtonMove buttonMove)
+    {
+        
+    }
+
+    public void ShowTextDetectIntersectionButtons()
+    {
+        textDetectIntersectionButtons.gameObject.SetActive(true);
+        buttonSave.gameObject.SetActive(false);
+    }
+
+    public void HideTextDetectIntersectionButtons()
+    {
+        textDetectIntersectionButtons.gameObject.SetActive(false);
+        buttonSave.gameObject.SetActive(true);
+    }
+
+    void Start ()
+    {
+        StartCoroutine(DelayRun());
+    }
+
+    IEnumerator DelayRun()
+    {
+        yield return new WaitForEndOfFrame();
+
+        HideTextDetectIntersectionButtons();
+        ConnectEvents();
+        LoadButtonsPositions();
+    }
+	
+	void Update () 
+	{
+	
+	}
+}
