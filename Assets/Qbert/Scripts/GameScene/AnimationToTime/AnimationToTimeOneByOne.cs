3459a24b9dbff7a99b167b18f38af7a043fb2b86
shda <arheuser@gmail.com>
2016-10-19 13:28:28 +0300

    Gift in work.

---
+++ Assets/Qbert/Scripts/GameScene/AnimationToTime/AnimationToTimeOneByOne.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Qbert.Scripts.GameScene;
+using Assets.Qbert.Scripts.GameScene.AnimationToTime;
+
+public class AnimationToTimeOneByOne : MonoBehaviour
+{
+    [System.Serializable]
+    public class TimeOneByOne
+    {
+        public ITime animation;
+        public float duration;
+        public float delayBefore;
+    }
+
+    public TimeOneByOne[] animations;
+
+    public void StartOneByOne()
+    {
+        if(animations == null)
+            return;
+
+        StartCoroutine(PlayAnimations());
+    }
+
+    IEnumerator PlayAnimations()
+    {
+        yield return null;
+
+        foreach (var animation in animations)
+        {
+            if(animation.delayBefore > 0)
+                yield return new WaitForSeconds(animation.delayBefore);
+
+            yield return StartCoroutine(PlayToTime(animation.animation, animation.duration));
+        }
+    }
+
+    public virtual IEnumerator PlayToTime(ITime iTime, float duration,bool isReverce = false)
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += (Time.deltaTime * iTime.timeScale) / duration;
+            iTime.time = t;
+            yield return null;
+        }
+
+        t = 1;
+
+        iTime.time = t;
+    }
+
+    void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Qbert/Scripts/GameScene/GiftBox/GiftBoxAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiftBoxAnimator : MonoBehaviour
+{
+    public AnimationToTimeOneByOne animationOpenBox;
+	void Start () 
+	{
+        animationOpenBox.StartOneByOne();
+
+    }
+
+	void Update () 
+	{
+	
+	}
+}
