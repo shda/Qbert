2fdd9618828297ae2d4579e37492bee1d7c88803
shda <arheuser@gmail.com>
2016-11-05 00:22:25 +0300

    Move swipe generic class to other file.

--- Assets/Qbert/Scripts/GUI/GUISettings/GuiSelectCharacter.cs
+++ Assets/Qbert/Scripts/GUI/GUISettings/GuiSelectCharacter.cs
@@ -10,26 +10,18 @@ using UnityEngine;
 
 namespace Assets.Qbert.Scripts.GUI.GUISettings
 {
-    public class GuiSelectCharacter : SwipeModel
+    public class GuiSelectCharacter : SwipeModel<QbertModel>
     {
         public GlobalConfigurationAsset globalConfigurationAsset;
-        public Transform rootModels;
         public TextMesh nameCurrentCharacter;
         public TextMesh descriptionCurrentCharacter;
         public CameraMenuController cameraMenuController;
         public GuiMainMenu rootMenu;
         public DiagonalSwipe diagonalSwipe;
-        public GameObject swipeHelpShow;
-
+        
         public Transform playImage;
-        public TextMesh  textPrice;
-
-        public float scaleModelNoFocus = 0.8f;
-        public float modelsOffset;
-
-        private QbertModel[] models;
-        private QbertModel currentModel;
-        private Coroutine scrollInWork;
+        public TextMesh textPrice;
+        public TextMesh textCountModels;
 
         public void OnFocusCameraToThis()
         {
@@ -37,6 +29,7 @@ namespace Assets.Qbert.Scripts.GUI.GUISettings
             cameraMenuController.OnCameraMoveSelectCharacter();
 
             ShowHelpHand(false);
+            UpdateCountModels();
 
             UnscaleTimer.StartDelay(1.0f, timer =>
             {
@@ -44,12 +37,32 @@ namespace Assets.Qbert.Scripts.GUI.GUISettings
             });
         }
 
+        public void UpdateCountModels()
+        {
+            int countAll = this.Count();
+            int countOpen = this.Count(x => x.isFree || x.isBuyed());
+
+            textCountModels.text = string.Format("{0}/{1}", countOpen, countAll);
+        }
+
+        public override void Scroll(bool isLeft)
+        {
+            base.Scroll(isLeft);
+
+
+            UpdateInfoFromModel(currentModel);
+
+            ShowHelpHand(false);
+            GlobalValues.isShowHelpHandToSelectCharacter = false;
+            GlobalValues.Save();
+        }
+
         public void ShowPrice(float price)
         {
             playImage.gameObject.SetActive(false);
             textPrice.gameObject.SetActive(true);
 
-            textPrice.text = string.Format("$ {0:##.##}", price);
+            textPrice.text = string.Format("$ {0:F2}", price);
         }
 
         public void ShowPlay()
@@ -63,12 +76,6 @@ namespace Assets.Qbert.Scripts.GUI.GUISettings
             cameraMenuController.OnCamaraMoveToRootMenu();
             ShowHelpHand(false);
         }
-
-        private void ShowHelpHand(bool isShow)
-        {
-            swipeHelpShow.gameObject.SetActive(isShow);
-        }
-
         public void OnButtonCharacterSelect()
         {
             Debug.Log("Select character: " + currentModel.nameCharacter);
@@ -83,40 +90,26 @@ namespace Assets.Qbert.Scripts.GUI.GUISettings
         public void InitScene()
         {
             var characters = globalConfigurationAsset.characters;
-            models = new QbertModel[characters.Length];
 
-            for (int i = 0; i < characters.Length; i++)
+            CreateModels(characters);
+
+            foreach (var model in this)
             {
-                var createCharacter = Instantiate(characters[i]);
-                models[i] = createCharacter;
+                model.booldeDead.gameObject.SetActive(false);
 
-                var tr = createCharacter.transform;
-                tr.SetParent(rootModels);
-                tr.localRotation = Quaternion.Euler(0,0,0);
-                tr.localPosition = new Vector3(i * modelsOffset , 0, 0);
-                tr.localScale = new Vector3(scaleModelNoFocus, scaleModelNoFocus, scaleModelNoFocus);
-
-                createCharacter.booldeDead.gameObject.SetActive(false);
-
-                if (!createCharacter.isFree && !createCharacter.isBuyed())
+                if (!model.isFree && !model.isBuyed())
                 {
-                    MeshRenderer[] renders = createCharacter.GetComponentsInChildren<MeshRenderer>();
+                    MeshRenderer[] renders = model.GetComponentsInChildren<MeshRenderer>();
                     foreach (var meshRenderer in renders)
                     {
-                        SetTransparentMaterial(meshRenderer , 
+                        SetTransparentMaterial(meshRenderer,
                             SwitchMaterialRenderingMode.BlendMode.Transparent, 0.3f);
                     }
                 }
             }
 
-            currentModel = models[0];
-            currentModel.transform.localScale = new Vector3(
-                1, 1, 1);
-
             UpdateInfoFromModel(currentModel);
         }
-
-
         private void SetTransparentMaterial(MeshRenderer meshRenderer,
             SwitchMaterialRenderingMode.BlendMode mode, float transparent)
         {
@@ -128,62 +121,18 @@ namespace Assets.Qbert.Scripts.GUI.GUISettings
             meshRenderer.material = material;
         }
 
-
-        public void Scroll(bool isLeft)
-        {
-            int index = Array.IndexOf(models ,currentModel);
-
-            if (isLeft && index >= models.Length - 1)
-            {
-                return;
-            }
-
-            if (!isLeft && index <= 0)
-            {
-                return;
-            }
-
-            QbertModel first = currentModel;
-            QbertModel second = isLeft ? models[index + 1] : models[index - 1];
-            ScrollToModel(first , second , 0.3f);
-            currentModel = second;
-            UpdateInfoFromModel(currentModel);
-
-            ShowHelpHand(false);
-            GlobalValues.isShowHelpHandToSelectCharacter = false;
-            GlobalValues.Save();
-        }
-
-
-        public void ScrollToModel(QbertModel first , QbertModel second , float speedMove)
-        {
-            float x = first.transform.localPosition.x - second.transform.localPosition.x;
-            x *= 0.3f;
-            var moveTo = rootModels.localPosition + new Vector3(0, 0, x);
-            scrollInWork = StartCoroutine(MovingLocalTransformTo(rootModels, moveTo, speedMove));
-            StartCoroutine(this.ScaleTranformTo(first.transform, scaleModelNoFocus, speedMove));
-            StartCoroutine(this.ScaleTranformTo(second.transform, 1.0f, speedMove));
-        }
-
         public void UpdateInfoFromModel(QbertModel model)
         {
             nameCurrentCharacter.text = model.nameCharacter;
             descriptionCurrentCharacter.text = model.description;
-        }
 
-
-        public void OnScrollEvent(DirectionMove.Direction dir)
-        {
-            if (scrollInWork == null)
+            if (model.isFree || model.isBuyed())
             {
-                if (dir == DirectionMove.Direction.DownLeft)
-                {
-                    Scroll(true);
-                }
-                else if (dir == DirectionMove.Direction.UpRight)
-                {
-                    Scroll(false);
-                }
+                ShowPlay();
+            }
+            else
+            {
+                ShowPrice(model.price);
             }
         }
 
@@ -194,19 +143,15 @@ namespace Assets.Qbert.Scripts.GUI.GUISettings
             SetFocusToGlobalModel();
 
         }
-
         public void SetFocusToGlobalModel()
         {
             var model = models.FirstOrDefault(x => x.nameCharacter == GlobalValues.currentModel);
             if (model != null)
             {
-                ScrollToModel(currentModel , model , 0.0f);
+                SetFocusToModel(model);
                 UpdateInfoFromModel(model);
-                currentModel = model;
             }
         }
-
-
         IEnumerator Test()
         {
             yield return new WaitForSeconds(2.0f);
@@ -215,20 +160,9 @@ namespace Assets.Qbert.Scripts.GUI.GUISettings
             Scroll(false);
         }
 
-        public IEnumerator MovingLocalTransformTo(Transform tr, Vector3 movingTo, float time)
+        void Update () 
         {
-            float distance = Vector3.Distance(tr.localPosition, movingTo);
-            float speedMoving = distance / time;
-
-            while (distance > 0.001f)
-            {
-                Vector3 move = Vector3.MoveTowards(tr.localPosition, movingTo, speedMoving * Time.deltaTime);
-                tr.localPosition = move;
-                distance = Vector3.Distance(tr.localPosition, movingTo);
-                yield return null;
-            }
-
-            scrollInWork = null;
+	
         }
     }
 }
--- Assets/Qbert/Scripts/GUI/GUISettings/SwipeModel.cs
+++ Assets/Qbert/Scripts/GUI/GUISettings/SwipeModel.cs
@@ -1,15 +1,133 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Qbert.Scripts;
+using Assets.Qbert.Scripts.GameScene;
+using Assets.Qbert.Scripts.GameScene.Characters;
+using Assets.Qbert.Scripts.Utils;
 
-public class SwipeModel : MonoBehaviour 
+public class SwipeModel<T> : MonoBehaviour, IEnumerable<T> where T : MonoBehaviour
 {
-	void Start () 
-	{
-	
-	}
+    public T currentModel;
+    public Transform rootModels;
 
-	void Update () 
-	{
-	
-	}
+    public float scaleModelNoFocus = 0.8f;
+    public float modelsOffset;
+
+    public GameObject swipeHelpShow;
+    public List<T> models;
+
+    private Coroutine scrollInWork;
+
+    protected void ShowHelpHand(bool isShow)
+    {
+        swipeHelpShow.gameObject.SetActive(isShow);
+    }
+
+    protected virtual void CreateModels(T[] prefabs)
+    {
+        var characters = prefabs;
+        models = new List<T>(characters.Length);
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            var createCharacter = Instantiate(characters[i]);
+            models.Add(createCharacter);
+
+            var tr = createCharacter.transform;
+            tr.SetParent(rootModels);
+            tr.localRotation = Quaternion.Euler(0, 0, 0);
+            tr.localPosition = new Vector3(i * modelsOffset, 0, 0);
+            tr.localScale = new Vector3(scaleModelNoFocus, scaleModelNoFocus, scaleModelNoFocus);
+
+        }
+
+        currentModel = models[0];
+        currentModel.transform.localScale = new Vector3(
+            1, 1, 1);
+    }
+
+    public virtual void Scroll(bool isLeft)
+    {
+        int index = models.IndexOf(currentModel);
+
+        if (isLeft && index >= models.Count - 1)
+        {
+            return;
+        }
+
+        if (!isLeft && index <= 0)
+        {
+            return;
+        }
+
+        var first = currentModel;
+        var second = isLeft ? models[index + 1] : models[index - 1];
+        ScrollToModel(first, second, 0.3f);
+        currentModel = second;
+
+        ShowHelpHand(false);
+        GlobalValues.isShowHelpHandToSelectCharacter = false;
+        GlobalValues.Save();
+    }
+
+    public void SetFocusToModel(T model)
+    {
+        ScrollToModel(currentModel, model, 0.0f);
+        currentModel = model;
+    }
+
+    public IEnumerator MovingLocalTransformTo(Transform tr, Vector3 movingTo, float time)
+    {
+        float distance = Vector3.Distance(tr.localPosition, movingTo);
+        float speedMoving = distance / time;
+
+        while (distance > 0.001f)
+        {
+            Vector3 move = Vector3.MoveTowards(tr.localPosition, movingTo, speedMoving * Time.deltaTime);
+            tr.localPosition = move;
+            distance = Vector3.Distance(tr.localPosition, movingTo);
+            yield return null;
+        }
+
+        scrollInWork = null;
+    }
+
+
+    public void ScrollToModel(T first, T second, float speedMove)
+    {
+        float x = first.transform.localPosition.x - second.transform.localPosition.x;
+        x *= 0.3f;
+        var moveTo = rootModels.localPosition + new Vector3(0, 0, x);
+        scrollInWork = StartCoroutine(MovingLocalTransformTo(rootModels, moveTo, speedMove));
+        StartCoroutine(this.ScaleTranformTo(first.transform, scaleModelNoFocus, speedMove));
+        StartCoroutine(this.ScaleTranformTo(second.transform, 1.0f, speedMove));
+    }
+
+    public void OnScrollEvent(DirectionMove.Direction dir)
+    {
+        if (scrollInWork == null)
+        {
+            if (dir == DirectionMove.Direction.DownLeft)
+            {
+                Scroll(true);
+            }
+            else if (dir == DirectionMove.Direction.UpRight)
+            {
+                Scroll(false);
+            }
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return models.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 }
